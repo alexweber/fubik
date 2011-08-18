76222ec10daa0e7722f371a0a230f9f0d545ff78
Alex Weber <alexweber15@gmail.com>
2011-08-18 18:41:31 -0300

    Implemented theme_menu_item() to add classes to menu items for better styling of custom admin menus

---
+++ template.php
@@ -0,0 +1,47 @@
+<?php
+
+/*
+ * 	Add custom classes to menu item
+ */
+
+function fubik_menu_item($link, $has_children, $menu = '', $in_active_trail = FALSE, $extra_class = NULL) {
+  $class = ($menu ? 'expanded' : ($has_children ? 'collapsed' : 'leaf'));
+  if (!empty($extra_class)) {
+    $class .= ' ' . $extra_class;
+  }
+  if ($in_active_trail) {
+    $class .= ' active-trail';
+  }
+  // New line added to get unique classes for each menu item
+  $css_class = fubik_id_safe(str_replace(' ', '_', strip_tags($link)));
+  return '<li class="' . $class . ' ' . $css_class . '">' . $link . $menu . "</li>\n";
+}
+
+/*
+ * 	Converts a string to a suitable html ID attribute.
+ *
+ * 	 http://www.w3.org/TR/html4/struct/global.html#h-7.5.2 specifies what makes a
+ * 	 valid ID attribute in HTML. This function:
+ *
+ * 	- Ensure an ID starts with an alpha character by optionally adding an 'n'.
+ * 	- Replaces any character except A-Z, numbers, and underscores with dashes.
+ * 	- Converts entire string to lowercase.
+ *
+ * 	@param $string
+ * 	  The string
+ * 	@return
+ * 	  The converted string
+ */
+
+function fubik_id_safe($string) {
+  // Strip accents
+  $accents = '/&([A-Za-z]{1,2})(tilde|grave|acute|circ|cedil|uml|lig);/';
+  $string = preg_replace($accents, '$1', htmlentities(utf8_decode($string)));
+  // Replace with dashes anything that isn't A-Z, numbers, dashes, or underscores.
+  $string = strtolower(preg_replace('/[^a-zA-Z0-9_-]+/', '-', $string));
+  // If the first character is not a-z, add 'n' in front.
+  if (!ctype_lower($string{0})) { // Don't use ctype_alpha since its locale aware.
+    $string = 'id' . $string;
+  }
+  return $string;
+}
